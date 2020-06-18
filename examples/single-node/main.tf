706c45ee6bc348314108e1ccabde67290b2c65a4
Mohammed Faizuddin <47523926+FaizuddinEverest@users.noreply.github.com>
2020-06-18 11:41:17 +0530

    Add output IP address to single-node example

--- examples/single-node/main.tf
+++ examples/single-node/main.tf
@@ -2,15 +2,20 @@ module "hazelcast_ecs_cluster" {
   source = "../../"
 
   name                       = "hazelcast"
-  hazelcast_version          = "latest"
+  hazelcast_version          = "3.12.7"
   hazelcast_container_cpu    = 512
   hazelcast_container_memory = 1024
   instance_type              = "t3.medium"
-  security_group_id          = aws_security_group.hazelcast-ecs-security-group.id
+  security_group_id          = aws_security_group.hazelcast_ecs_security_group.id
   subnet_id                  = module.dynamic-subnets.public_subnet_ids[0]
 
   tags = {
     Environment = "Testing"
     CreatedBy = "Terraform"
   }
+}
+
+output "public_ip" {
+  description = "Public IP Address"
+  value = module.hazelcast_ecs_cluster.instance_public_ip
 }
--- examples/single-node/prerequisites.tf
+++ examples/single-node/prerequisites.tf
@@ -32,7 +32,7 @@ module "dynamic-subnets" {
   tags               = local.default_tags
 }
 
-resource "aws_security_group" "hazelcast-ecs-security-group" {
+resource "aws_security_group" "hazelcast_ecs_security_group" {
   name   = local.name
   vpc_id = module.vpc.vpc_id
 
