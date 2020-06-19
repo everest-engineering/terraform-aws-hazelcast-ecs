68a346e512ed86549c0ea7be2154cba4e85de4be
Mohammed Faizuddin <47523926+FaizuddinEverest@users.noreply.github.com>
2020-06-19 23:36:27 +0530

    Add ECS information to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
+output "ecs_cluster_arn" {
+  description = "ARN of ECS Cluster"
+  value       = aws_ecs_cluster.hazelcast_ecs.arn
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS Cluster"
+  value       = aws_ecs_cluster.hazelcast_ecs.name
+}
+
 output "instance_public_ip" {
   description = "Public IP of the ECS EC2 instance"
-  value = aws_instance.hazelcast_instance.public_ip
+  value       = aws_instance.hazelcast_instance.public_ip
+}
+
+output "instance_private_ip" {
+  description = "Private IP of the ECS EC2 instance"
+  value       = aws_instance.hazelcast_instance.private_ip
 }
