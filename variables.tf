f2f638c12d4efbd454d9928247c588379bcbfbf2
Mohammed Faizuddin <47523926+FaizuddinEverest@users.noreply.github.com>
2020-06-27 14:21:22 +0530

    Make certain variables required

--- variables.tf
+++ variables.tf
@@ -1,39 +1,31 @@
 variable "name" {
   description = "The name of the deployment"
-  default = ""
 }
 
 variable "tags" {
   description = "Tags for the created resources"
-  default = ""
 }
 
 variable "hazelcast_version" {
   description = "Hazelcast version to deploy"
-  default = ""
 }
 
 variable "hazelcast_container_cpu" {
   description = "Hazelcast container CPU units"
-  default = ""
 }
 
 variable "hazelcast_container_memory" {
   description = "Hazelcast container memory"
-  default = ""
 }
 
 variable "instance_type" {
   description = "EC2 Instance type to launch for ECS"
-  default = ""
 }
 
 variable "security_group_id" {
   description = "EC2 Security Group ID"
-  default = ""
 }
 
 variable "subnet_id" {
   description = "EC2 Subnet ID"
-  default = ""
 }
