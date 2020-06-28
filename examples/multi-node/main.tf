8cd2f9876d09068f7f24b23293f093318f7f64a8
Mohammed Faizuddin <47523926+FaizuddinEverest@users.noreply.github.com>
2020-06-28 08:48:06 +0530

    Update examples to use hazelcast_discovery_tag_key

--- examples/multi-node/main.tf
+++ examples/multi-node/main.tf
@@ -1,16 +1,17 @@
 module "hazelcast_ecs_cluster" {
   source = "../../"
 
-  name                       = "hazelcast"
-  region                     = "ap-southeast-1"
-  hazelcast_version          = "3.12.7"
-  hazelcast_container_cpu    = 512
-  hazelcast_container_memory = 2000
-  instance_type              = "t3.medium"
-  instance_count             = 2
-  members_count              = 2
-  security_group_id          = aws_security_group.hazelcast_ecs_security_group.id
-  subnet_id                  = module.dynamic-subnets.public_subnet_ids[0]
+  name                        = "hazelcast"
+  region                      = "ap-southeast-1"
+  hazelcast_version           = "3.12.7"
+  hazelcast_container_cpu     = 512
+  hazelcast_container_memory  = 1800
+  instance_type               = "t3.medium"
+  instance_count              = 2
+  members_count               = 2
+  security_group_id           = aws_security_group.hazelcast_ecs_security_group.id
+  subnet_id                   = module.dynamic-subnets.public_subnet_ids[0]
+  hazelcast_discovery_tag_key = "Environment"
 
   tags = {
     Environment = "Testing"
--- examples/single-node/main.tf
+++ examples/single-node/main.tf
@@ -2,13 +2,14 @@ module "hazelcast_ecs_cluster" {
   source = "../../"
   region = "ap-southeast-1"
 
-  name                       = "hazelcast"
-  hazelcast_version          = "3.12.7"
-  hazelcast_container_cpu    = 512
-  hazelcast_container_memory = 1024
-  instance_type              = "t3.medium"
-  security_group_id          = aws_security_group.hazelcast_ecs_security_group.id
-  subnet_id                  = module.dynamic-subnets.public_subnet_ids[0]
+  name                        = "hazelcast"
+  hazelcast_version           = "3.12.7"
+  hazelcast_container_cpu     = 512
+  hazelcast_container_memory  = 1024
+  instance_type               = "t3.medium"
+  security_group_id           = aws_security_group.hazelcast_ecs_security_group.id
+  subnet_id                   = module.dynamic-subnets.public_subnet_ids[0]
+  hazelcast_discovery_tag_key = "Environment"
 
   tags = {
     Environment = "Testing"
